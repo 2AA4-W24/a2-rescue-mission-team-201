3124e016625a6535badce3d6b3a93c2b361ddb3f
aaronloh16 <aaronloh04@gmail.com>
2024-03-12 20:08:10 -0400

    closes #36; Added test for extras class

--- src/test/java/ca/mcmaster/se2aa4/island/team201/ExtrasTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team201/ExtrasTest.java
@@ -1,17 +1,30 @@
 package ca.mcmaster.se2aa4.island.team201;
 import org.json.JSONObject;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 
 
-// public class ExtrasTest {
-//     @Test
-//     void updatesEchoResponsesCorrectly() {
-//         Extras extras = new Extras();
-//         JSONObject echoInfo = new JSONObject("{\"range\": 5, \"found\": \"tree\"}");
-//         extras.updateState(echoInfo, "echo");
-//         assertEquals(1, extras.getEchos().size());
-//         assertEquals("tree", extras.mostRecentEcho().found());
-//     }
+ public class ExtrasTest {
+     @Test
+     void testEchoActionUpdates() {
+         Extras extras = new Extras();
+         Action echoAction = new Action("echo", "N");
 
-// }
+         JSONObject echoInfo = new JSONObject()
+                 .put("range", 5)
+                 .put("found", "tree");
+         Coordinate location = new Coordinate(0,0);
+
+         extras.updateState(echoInfo, echoAction, location);
+
+         // Assertions
+         assertFalse(extras.getEchos().isEmpty(), "Echo list shouldn't be empty after processing echo");
+         Echo mostRecentEcho = extras.lastEcho();
+         assertEquals(5, mostRecentEcho.range(), "The range of recent echo should be 5");
+         assertEquals("tree", mostRecentEcho.found(), "should be 'tree'");
+         assertEquals("N", mostRecentEcho.direction(), " direction should be 'N'.");
+     }
+
+ }
