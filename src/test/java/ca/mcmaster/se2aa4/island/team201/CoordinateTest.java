8b701ea8ff2ec356bcf9ce7772771b3c15e4db82
CheukmanZhou <cheukmanzhou@gmail.com>
2024-03-14 13:57:28 -0400

    Implemented the skeleton for the CoordinateTest

---
+++ src/test/java/ca/mcmaster/se2aa4/island/team201/CoordinateTest.java
@@ -0,0 +1,8 @@
+package ca.mcmaster.se2aa4.island.team201;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CoordinateTest {
+    
+}
