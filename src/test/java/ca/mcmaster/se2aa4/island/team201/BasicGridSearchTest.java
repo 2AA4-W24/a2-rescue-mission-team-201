c9d29c66290665bb3cf68915d168b24e14cf86f6
CheukmanZhou <cheukmanzhou@gmail.com>
2024-03-23 16:55:09 -0400

    added setUp for BasicGridSearchTest

--- src/test/java/ca/mcmaster/se2aa4/island/team201/BasicGridSearchTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team201/BasicGridSearchTest.java
@@ -1,5 +1,22 @@
 package ca.mcmaster.se2aa4.island.team201;
 
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import ca.mcmaster.se2aa4.island.team201.phases.BasicGridSearch;
+
 public class BasicGridSearchTest {
-    
+    private ActionExecutor mockExecutor;
+    private Interpreter mockInterpreter;
+    private BasicGridSearch basicGridSearch;
+
+    @BeforeEach
+    void setUp() {
+        mockExecutor = mock(ActionExecutor.class);
+        mockInterpreter = mock(Interpreter.class);
+        basicGridSearch = new BasicGridSearch(mockExecutor, mockInterpreter);
+    }
 }
