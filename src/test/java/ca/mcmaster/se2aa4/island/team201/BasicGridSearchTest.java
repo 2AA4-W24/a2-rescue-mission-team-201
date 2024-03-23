94950ef2f28bfc907353424c5c4013b97aa2a60d
CheukmanZhou <cheukmanzhou@gmail.com>
2024-03-23 17:10:12 -0400

    added testInitialization for BasicGridSearchTest

--- src/test/java/ca/mcmaster/se2aa4/island/team201/BasicGridSearchTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team201/BasicGridSearchTest.java
@@ -1,7 +1,10 @@
 package ca.mcmaster.se2aa4.island.team201;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,5 +21,13 @@ public class BasicGridSearchTest {
         mockExecutor = mock(ActionExecutor.class);
         mockInterpreter = mock(Interpreter.class);
         basicGridSearch = new BasicGridSearch(mockExecutor, mockInterpreter);
+    }
+
+    @Test
+    public void testInitialization() {
+        JSONObject info = new JSONObject();
+        basicGridSearch.initialize(info);
+        // Verify that interpreter's directionOfLines is set correctly
+        verify(mockInterpreter).setDirectionOfLines(any());
     }
 }
